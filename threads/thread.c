4d55581bd65cbf705374ed6afc39c3f247a6f0ac
Jack-Rentz <jackrentz@utexas.edu>
2018-09-30 20:44:26 +0000

    commit

--- threads/thread.c
+++ threads/thread.c
@@ -473,7 +473,7 @@ is_thread (struct thread *t)
   else*/
   //printf("It wasn't NULL, the thread-magic number is %d, and it should be %d\n", t->magic, THREAD_MAGIC);
   bool isthread = t != NULL && t->magic == THREAD_MAGIC;
-  while (!isthread);
+  printf("This is stupid\n");
   ASSERT(isthread);
   return isthread;
 }
