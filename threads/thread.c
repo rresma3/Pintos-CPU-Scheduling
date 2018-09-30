f5dcf18a0d389128af5deef0dfc459bfea33f942
Jack-Rentz <jackrentz@utexas.edu>
2018-09-30 21:31:50 +0000

    commit

--- threads/thread.c
+++ threads/thread.c
@@ -102,9 +102,6 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
-  
-  //
-  sema_init (&(thread_current()->block), 0);
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
@@ -495,6 +492,8 @@ init_thread (struct thread *t, const char *name, int priority)
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
   t->magic = THREAD_MAGIC;
+  
+  sema_init (t->block), 0);
 
   old_level = intr_disable();
   list_push_back (&all_list, &t->allelem);
