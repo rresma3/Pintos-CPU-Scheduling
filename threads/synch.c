f8775afa9b17aee0d20f901971665cd66db0d31e
EC2 Default User <ec2-user@ip-172-31-9-197.us-east-2.compute.internal>
2018-10-05 21:57:38 +0000

    commit

--- threads/synch.c
+++ threads/synch.c
@@ -117,7 +117,9 @@ sema_up (struct semaphore *sema)
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
   sema->value++;
+  
   intr_set_level (old_level);
+  thread_yeild ();
 }
 
 static void sema_test_helper (void *sema_);
