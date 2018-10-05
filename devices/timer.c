73cf428d85ba0530ce5582235960f53e6c2cd662
EC2 Default User <ec2-user@ip-172-31-9-197.us-east-2.compute.internal>
2018-10-05 23:24:59 +0000

    commit

--- devices/timer.c
+++ devices/timer.c
@@ -206,7 +206,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
       
       // unblock the current thread after removing from blocked list
       sema_up(&(curr_thread->block));
-      sema_down(&(curr_thread->block));
+      sema_try_down(&(curr_thread->block));
       
       if (!list_empty(&blocked_list))  // are there still blocked threads?
       {
