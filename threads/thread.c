e049efc377d0707f79ff75f8d02d2317f4f69d67
Jack-Rentz <jackrentz@utexas.edu>
2018-10-06 18:16:57 +0000

    commit

--- threads/thread.c
+++ threads/thread.c
@@ -404,7 +404,7 @@ thread_donate_priority(struct thread *t, int new_priority)
 {
    // donation : change only current priority
   t->priority = new_priority;
-  list_push_front(thread_current ()->priority_donors, &t);
+  list_push_front(thread_current ()->priority_donors, t->elem);
   
   // if current thread gets its priority decreased, then yield
   // (foremost entry in ready_list shall have the highest priority)
