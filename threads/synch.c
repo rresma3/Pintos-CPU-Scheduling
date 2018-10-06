dfb34800e3af98b840e12b3bd4058c0bb24b463c
EC2 Default User <ec2-user@ip-172-31-9-197.us-east-2.compute.internal>
2018-10-06 01:06:07 +0000

    commit

--- threads/synch.c
+++ threads/synch.c
@@ -333,8 +333,8 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  //list_push_back (&cond->waiters, &waiter.elem);
-  list_insert_ordered(&cond->waiters, &waiter.elem, list_priority_sort, NULL);
+  list_push_back (&cond->waiters, &waiter.elem);
+  //list_insert_ordered(&cond->waiters, &waiter.elem, list_priority_sort, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
